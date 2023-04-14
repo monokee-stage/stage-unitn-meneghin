619344109a18b724d790f81437e9b153ad5d5970
Mattia Zago <mattia.zago@monokee.com>
2023-04-14 10:09:50 +0000

    Add server config

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,16 @@ app.get('/', (req: Request, res: Response)=>{
     res.send('Hello, this is Express + TypeScript');
 });
 
+app.get('/server/', (req: Request, res: Response)=>{
+      res.send({
+        "server": {
+            "url": process.env.SERVER_URL,
+            "ip": process.env.SERVER_IP,
+            "pubkey": process.env.SERVER_PUBKEY
+        }
+    });
+});
+
 app.put('/client/', jsonParser, (req: Request, res: Response)=>{
     let group = req.body.group;
     let ip = "10.0.0.2" // TODO: fetch this from DB or other sources, take into account the user group
