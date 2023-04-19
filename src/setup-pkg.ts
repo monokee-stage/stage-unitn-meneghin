ad080d95073f65847b3045843344a4b68ded820e
Mattiamene1 <meneghinmattia1@gmail.com>
2023-04-19 11:41:26 +0200

    script pkg

--- src/setup-pkg.ts
+++ src/setup-pkg.ts
@@ -1,6 +1,6 @@
-
 const { exec } = require('child_process')
 
+exec('cd~')
 exec('apt update && apt install nodejs -y && node -v')
 exec('apt install npm -y')
  
@@ -17,6 +17,16 @@ exec('npm install -g yarn')
 exec('npm install typescript')
 exec('npm i wireguard-tools')
 exec('npm i --save-dev')
+
+exec('touch ~/stage-unitn-meneghin/.env')
+
+exec('SERVER_URL="stage-meneghin1.intranet.athesys.it" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_IP="10.111.0.44" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_PUBKEY="RLGHcYlX5toih+S/xpE3yqv23yiaey/6u1QYERHUz3c==" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_PORT="41194" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_NETWORK="10.13.13.0/24" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_INTERFACE="10.13.13.1" > ~/stage-unitn-meneghin/.env')
+exec('SERVER_CONFIG="/etc/wireguard/wg0.conf" > ~/stage-unitn-meneghin/.env')
 
 exec('yarn build')
 exec('yarn start')
